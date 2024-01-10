71172b83e8cd8c695123ab9aa8eb18f0bf5e6da4
jeongdxxn <dxxn.jeong@gmail.com>
2024-01-10 16:56:40 +0900

    fix: api error

--- src/components/map/KakaoMap.tsx
+++ src/components/map/KakaoMap.tsx
@@ -9,14 +9,15 @@ declare global {
 export default function KakaoMap() {
 	const mapRef = useRef<HTMLDivElement>(null);
 	useEffect(() => {
-		window.kakao.maps.load(() => {
+		const { kakao } = window;
+		kakao.maps.load(() => {
 			const options = {
 				//지도를 생성할 때 필요한 기본 옵션
-				center: new window.kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+				center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
 				level: 5, //지도의 레벨(확대, 축소 정도)
 			};
 
-			const map = new window.kakao.maps.Map(mapRef.current, options); //지도 생성 및 객체 리턴
+			const map = new kakao.maps.Map(mapRef.current, options); //지도 생성 및 객체 리턴
 		});
 	}, []);
 	return <div id="map" ref={mapRef} className="h-full w-full"></div>;
