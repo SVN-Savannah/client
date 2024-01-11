ab6f1f65b281405135a91bb6b744525ca1361f4b
gimseonjin616 <66009926+gimseonjin@users.noreply.github.com>
2024-01-11 23:18:51 +0900

    fix: Delete Export Infront of authOptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,7 @@ import GoogleProvider from 'next-auth/providers/google';
 import { SupabaseAdapter } from '@auth/supabase-adapter';
 import { Adapter } from 'next-auth/adapters';
 
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
 	providers: [
 		KakaoProvider({
 			clientId: process.env.KAKAO_CLIENT_ID ?? '',
@@ -29,27 +29,5 @@ export const authOptions: NextAuthOptions = {
 	},
 };
 
-const handler = NextAuth({
-	providers: [
-		KakaoProvider({
-			clientId: process.env.KAKAO_CLIENT_ID ?? '',
-			clientSecret: process.env.KAKAO_CLIENT_SECRET ?? '',
-		}),
-		NaverProvider({
-			clientId: process.env.NAVER_CLIENT_ID ?? '',
-			clientSecret: process.env.NAVER_CLIENT_SECRET ?? '',
-		}),
-		GoogleProvider({
-			clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-			clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-		}),
-	],
-	adapter: SupabaseAdapter({
-		url: process.env.SUPABASE_URL ?? "",
-		secret: process.env.SUPABASE_SECRET ?? "",
-	}) as Adapter,
-	pages: {
-		signIn: '/login',
-	},
-});
+const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
