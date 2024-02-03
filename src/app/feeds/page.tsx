ad64ee7e4cf2f44e2300c334088b695e4526ff7d
jeongdxxn <dxxn.jeong@gmail.com>
2024-02-04 01:00:59 +0900

    fix: disable unused components

--- src/app/feeds/page.tsx
+++ src/app/feeds/page.tsx
@@ -3,7 +3,8 @@ import FeedContainer from '@/components/feed/FeedContainer';
 export default function feedsPage() {
 	return (
 		<main className="flex h-full w-full items-start justify-center bg-white">
-			<FeedContainer />
+			{/* @TODO 검색 내용(마커리스트) 표시? */}
+			{/* <FeedContainer /> */}
 		</main>
 	);
 }
